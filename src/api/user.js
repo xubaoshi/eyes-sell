c63a3a1eba7593f149b53132f3207830ec793305
xubaoshi <948869516@qq.com>
2018-09-07 11:14:41 +0800

    edit code

--- src/api/user.js
+++ src/api/user.js
@@ -13,12 +13,6 @@ export default class user extends base {
     store.save('user', userInfo)
   }
 
-  // 更新用户
-  static async updateUser(param) {
-    const url = `${this.baseUrl}${patientApi.user.update}`
-    return await this.post(url, param)
-  }
-
   // 推荐二维码
   static async qrcode(param) {
     const url = `${this.baseUrl}${patientApi.user.share.qrcode}`
@@ -27,55 +21,16 @@ export default class user extends base {
     return qrcode
   }
 
-  // 推荐 我的发展的用户
-  static async users(param) {
-    const url = `${this.baseUrl}${patientApi.user.share.users}`
-    const arr = await this.get(url, param)
-    arr.forEach(item => {
-      item['dateFormat'] = lang.dateFormat(item.date, 'yyyy-MM-dd')
-    })
-    return arr
-  }
-
   // 收入明细
   static income(param) {
     const url = `${this.baseUrl}${patientApi.user.income}`
     return new Page(url, this.processData.bind(this))
   }
 
-  // 提现请求
-  static async cashRequest(param) {
-    const url = `${this.baseUrl}${patientApi.user.cash.request}`
-    return await this.post(url, param)
-  }
-  // 获取我添加的银行卡
-  static async myCards(param) {
-    const url = `${this.baseUrl}${patientApi.user.cash.myCards}`
-    const arr = await this.get(url, param)
-    return arr
-  }
-  // 全部银行卡
-  static async allCards(param) {
-    const url = `${this.baseUrl}${patientApi.user.cash.allCards}`
-    const arr = await this.get(url, param)
-    return arr
-  }
-  // 提现历史
-  static cashHistory(param) {
-    const url = `${this.baseUrl}${patientApi.user.cash.history}`
-    return new Page(url, this.processData.bind(this))
-  }
-
-  // 新增提现银行
-  static async addCard(param) {
-    const url = `${this.baseUrl}${patientApi.user.cash.addCard}`
-    return await this.post(url, param)
-  }
-
   // 发展用户
   static userList() {
     const url = `${this.baseUrl}${patientApi.user.myUser}`
-    return new Page(url)
+    return new Page(url, this.processData.bind(this))
   }
 
   // 发展医生
